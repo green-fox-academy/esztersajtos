960e3fc36d1799d1cacd0547b245b8de950d59de
Eszter <sajtoseszter@gmail.com>
2018-02-15 12:45:16 +0100

    add Two numbers

---
+++ Week01/Day4/indexintro.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+console.log('My name is Eszter');
+console.log('I am ' + 108 + ' years old');
+console.log('Hight: ' + 1.72 + ' m');
