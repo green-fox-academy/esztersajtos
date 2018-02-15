47f7a51fee343391bc6605e051b07d0fbb010a9d
Eszter <sajtoseszter@gmail.com>
2018-02-15 14:54:54 +0100

    add mass

---
+++ Week01/Day4/indexmass.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+
+let total: number = massInKg / heightInM;
+
+console.log(total / heightInM);
+console.log((massInKg / heightInM) / heightInM);
