3cc68c90e7f430e5ec29fb1f9aa88793d3b04066
Eszter <sajtoseszter@gmail.com>
2018-02-15 15:48:43 +0100

    add basic

---
+++ Week01/Day4/indexbasic.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let myName: string = 'Eszter';
+    console.log(myName);
+
+let age: number = 108;
+    console.log(age);
+
+let hight: number = 1.72;
+    console.log(hight);
