02fe294ab50c30437aaba42e1f1115b575780889
Eszter <sajtoseszter@gmail.com>
2018-02-19 14:58:41 +0100

    add factorio

---
+++ Week02/Day1/factorio.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+
+// -  Create a function called `factorio`
+//    that returns it's input's factorial
+
+function factorial(n) {
+    return (n != 1) ? n * factorial(n - 1) : 1;
+  }
+  
+  console.log( factorial(4));
