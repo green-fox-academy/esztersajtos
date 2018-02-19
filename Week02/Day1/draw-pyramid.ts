c6d3fa81454995cb222fe400e24161ed58f2df46
Eszter <sajtoseszter@gmail.com>
2018-02-19 12:03:31 +0100

    add doubling

---
+++ Week02/Day1/draw-pyramid.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// -  Create a variable named `baseNum` and assign the value `123` to it
+// -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// -  Log the result of `doubling(baseNum)` to the console
+
+
+function doubling(number){
+    let baseNum: number = 123;
+    return (baseNum * 2 * Math.PI);
+
+}
+console.log(doubling);
