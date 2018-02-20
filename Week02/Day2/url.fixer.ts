546eb9fe77e6167e33e858abc9b803a51080c9b1
Eszter <sajtoseszter@gmail.com>
2018-02-20 13:17:14 +0100

    add url.fixer

---
+++ Week02/Day2/url.fixer.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Create a function that can reverse a string, which is passed as the parameter
+'use strict';
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+    let urlRepl = url.replace(/bots/i, 'odds');
+    urlRepl = url.replace(/https/i, 'https:');
+
+
+console.log(urlRepl);
+
