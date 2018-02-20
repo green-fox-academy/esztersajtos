40495dc4ef8a5fd943cfb11382cec1626a7150cf
Eszter <sajtoseszter@gmail.com>
2018-02-20 13:56:18 +0100

    add todo.list

--- Week02/Day2/takes.longer.ts
+++ Week02/Day2/takes.longer.ts
@@ -4,9 +4,14 @@
 'use strict';
 // When saving this quote a disk error has occured. Please fix it.
 // Add "always takes longer than" to between the words "It" and "you"
+/*
 let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
 let sliced1 = quote.slice(0,21);
 let sliced2 = quote.slice(21);
 
-console.log(sliced1 + 'always takes longer than ' + sliced2);
+console.log(sliced1 + 'always takes longer than ' + sliced2); */
 
+let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
+let index = quote.indexOf('It');
+let index2 = quote.indexOf('you');
+console.log(index + 'always takes longer than ' + index2);
---
+++ Week02/Day2/todo.print.ts
@@ -0,0 +1,17 @@
+'use strict';
+// Add "My todo:" to the beginning of the todoText
+// Add " - Download games" to the end of the todoText
+// Add " - Diablo" to the end of the todoText but with indention
+// Expected outpt:
+// My todo:
+//  - Buy milk
+//  - Download games
+//      - Diablo
+let todoText: string = ' - Buy milk\n';
+let todoMy: string = 'My todo:\n';
+let todoGame: string = ' - Download games\n      - Diablo'
+
+
+console.log(todoMy, todoText, todoGame, );
+
+
