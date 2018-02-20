5cec70a5da78d5da7dea66aaee8df7e73611831a
Eszter <sajtoseszter@gmail.com>
2018-02-20 13:27:47 +0100

    add takes.longer

---
+++ Week02/Day2/takes.longer.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+// Create a function that can reverse a string, which is passed as the parameter
+'use strict';
+// When saving this quote a disk error has occured. Please fix it.
+// Add "always takes longer than" to between the words "It" and "you"
+let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
+let sliced1 = quote.slice(0,21);
+let sliced2 = quote.slice(21);
+
+console.log(sliced1 + 'always takes longer than ' + sliced2);
+
