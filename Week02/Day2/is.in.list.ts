244eb68f48f19987599243954c8a56dd31600fcb
Eszter <sajtoseszter@gmail.com>
2018-02-21 01:24:25 +0100

    add is.it.list again

--- Week02/Day2/is.in.list.ts
+++ Week02/Day2/is.in.list.ts
@@ -3,13 +3,13 @@
 // Create a function that accepts 'listOfNumbers' as an input
 // it should return "true" if it contains all, otherwise "false"
 let listOfNumbers: number[] = [2, 4, 6, 8, 10, 12, 14, 16];
-function checkNums(input:number[]){
+function checkNums(input:number[]) {
     for (let i = 0; i < input.length; i++){
-        if (input[i] === 4 || 8 || 12 || 16){
+        if (input[i] === 4 || input[i] === 8 || input[i] === 12 || input[i] === 16){
             return true
-        }        
-        return false  
+        }          
     }
+    return false
 }
 console.log(checkNums(listOfNumbers));
 
