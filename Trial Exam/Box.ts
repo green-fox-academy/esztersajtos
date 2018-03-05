82f740c3439d4381ceb5af1d87208a9b531bf155
Eszter <sajtoseszter@gmail.com>
2018-03-05 11:57:24 +0100

    update Box.ts

--- Trial Exam/Box.ts
+++ Trial Exam/Box.ts
@@ -4,46 +4,29 @@
 // It should have a method called `getVolume` that returns the cuboid's volume
 'use strict'
 
-// class Cuboid {
-// 	length: number;
-// 	height: number;
-// 	width: number;
+class Cuboid {
+	length: number;
+	height: number;
+	width: number;
 
-// 	constructor (l, h, w) {
-// 		this.length = l;
-// 		this.height = h;
-// 		this.width = w;
-// 	}
+	constructor (l, h, w) {
+		this.length = l;
+		this.height = h;
+		this.width = w;
+	}
 
-// 	getSurface(){
-// 		return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height)); 
-// 	}
-// }
+	getSurface() {
+		return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height)); 
+	}
 
-// let cuboid1 = new Cuboid(20, 30, 10);
-// cuboid1.getSurface();
-// console.log(cuboid1);
-
-'use strict'
-
-class Sharpie {
-    color: string;
-    width: number;
-    inkAmount: number = 100;
-
-    constructor(color: string, width: number, inkAmount: number){
-        this.color = color;
-        this.width = width;
-        this.inkAmount = inkAmount;
-
-    }
-    
-    use(){
-        return this.inkAmount -= 1;
-    }
+	getVolume() {
+		return (this.length * this.height * this.width);
+	}
 }
 
+let cuboid1 = new Cuboid(20, 30, 10);
+cuboid1.getSurface();
+cuboid1.getVolume();
+console.log(cuboid1.getSurface());
+console.log(cuboid1.getVolume());
 
-let sharpie1 = new Sharpie('blue', 12, 20);
-sharpie1.use();
-console.log(sharpie1);
