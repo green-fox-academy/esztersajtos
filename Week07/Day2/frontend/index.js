3896168dcf2930a582ce7b08eea9e81c8b3bd2dc
Eszter <sajtoseszter@gmail.com>
2018-04-03 18:09:44 +0200

    add Do Until

--- Week07/Day2/frontend/index.js
+++ Week07/Day2/frontend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const port = 3000;
 
-app.use('/static', express.static('static'));
+app.use('/assets', express.static('assets'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
@@ -22,6 +25,56 @@ app.get('/doubling', (req, res) => {
     }
   }
   res.json(response);
+});
+
+app.get('/greeter', (req, res) => {
+  const title = req.query.title;
+  if (req.query.name === undefined) {
+    response = {
+    "error": "Please provide a name!"
+    }
+  } else if (req.query.title === undefined) {
+    response = {
+      "error": "Please provide a title!",
+    };
+  } else {
+    response = {
+      welcome_message: `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`,
+    }
+  }
+  res.json(response);
+});
+
+app.get('/appenda/:appendable', (req, res) => {
+  res.json({
+    appended: req.params.appendable + "a",
+  })
+});
+
+app.post('/dountil/:what', (req, res) => {
+  let what = req.params.what;
+  if (req.body.until === undefined) {
+    res.json({ 
+      error: 'Please provide a number!',
+    });
+    return;
+  } else if (what === 'sum') {
+    let myNumber = 0;
+    for (let i = 1; i <= req.body.until; i++) {
+      myNumber += i;
+    }
+    res.json({
+      result: myNumber,
+    });
+  } else if (what === 'factor') {
+    let myNumber = 1;
+    for (let i = 1; i <= req.body.until; i++) {
+      myNumber = myNumber * i;
+    }
+    res.json({
+      result: myNumber,
+    });
+  }
 });
 
 app.listen(port, () => {
