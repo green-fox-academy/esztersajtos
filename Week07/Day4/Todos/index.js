63cb3e6ad311727db05c4c5d750a10e11e31e7f2
Eszter <sajtoseszter@gmail.com>
2018-04-05 13:14:08 +0200

    add Todos

---
+++ Week07/Day4/Todos/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+const todos = [
+  'get up',
+  'survive',
+  'go back to bed',
+];
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: 'Todos',
+    todos: todos,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
