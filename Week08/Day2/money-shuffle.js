a44c9fcb98d0faf70502c67e7a881f1ba253f0f3
Eszter <sajtoseszter@gmail.com>
2018-04-10 16:06:01 +0200

    add Money-shuffle

---
+++ Week08/Day2/money-shuffle.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const Panama = {
+  cash: 0,
+  name: 'Panama',
+  tax: '1%',
+  deposit: function(amt) {
+    this.cash += amt;
+  }
+};
+
+const Cyprus = {
+  cash: 0,
+  name: 'Cyprus',
+  tax: '5%',
+  deposit: function(amt) {
+    this.cash += amt;
+  }
+};
+
+const Shuffler = {
+  cash: 10000,
+  pickNumber: 1,
+  pick: function() {
+    if (this.pickNumber % 2 === 0) {
+    Panama.deposit(1000 * 0.1);
+    console.log('Panama got 1000.');
+    } else {
+      Cyprus.deposit(1000 * 0.5);
+      console.log('Cyprus got 1000.');
+    }
+    this.cash -= this.deposit;
+    this.pickNumber++;
+  }
+};
+
+Shuffler.pick(); // prints Panama got 1000
+Shuffler.pick(); // prints Cyprus got 1000
+Shuffler.pick(); // prints Panama got 1000
+Shuffler.pick(); // prints Cyprus got 1000
+
+console.log(Panama.cash); // 2000 
+console.log(Cyprus.cash); // 2000 
