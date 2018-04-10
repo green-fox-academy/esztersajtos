0f04f92c724f440439866895705c4ae0e313b2c0
Eszter <sajtoseszter@gmail.com>
2018-04-10 13:50:57 +0200

    add multiplier, prison-break

---
+++ Week08/Day2/prison-break.js
@@ -0,0 +1,34 @@
+// Create a prison function, that has your name as a private fugutive variable
+// The function should return an object that has two methods:
+//  - visit() // logs "[yourname] is visited [x] time(s)" to the console.
+//    - the [x] times displayes the numbers the function is called
+//    - If the fugitive variable is empty, then display "Nobody is here anymore"
+//  - escape() // logs "BREAKING NEWS, [yourname] escaped the prison" to the console.
+//    - it should empties the fugitive variable
+
+function prison(myName) {
+  let visit = 0;
+  let name = myName;
+
+  return {
+    visit: function() {
+      visit ++;
+      if (myName === undefined) {
+        console.log('Nobody is here anymore.');
+      } else {
+        console.log(`${myName} is visited ${visit}`);
+      }
+    },
+    escape: function() {
+      console.log(`BREAKING NEWS, ${myName} escaped the prison!`);
+      myName = undefined;
+    }
+  }
+};
+
+const alcatraz =  prison('Evil Panda');
+
+alcatraz.visit(); // Evil Panda is visited 1 time(s)
+alcatraz.visit(); // Evil Panda is visited 2 time(s)
+alcatraz.escape(); // BREAKING NEWS, Evil Panda escaped the prison
+alcatraz.visit(); // Nobody is here anymore
