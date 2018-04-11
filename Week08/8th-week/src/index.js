3aad0000ccd30bfde96d7523d8038ef3ecb3d6fe
Eszter <sajtoseszter@gmail.com>
2018-04-11 18:57:46 +0200

    add engine

--- Week08/8th-week/src/index.js
+++ Week08/8th-week/src/index.js
@@ -1,13 +1,18 @@
 const express = require ('express');
+const path = require ('path');
 
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index', {
+    title: 'Dashboard',
+  });
 });
 
 app.listen(PORT, () => {
-  console.log('App is running on port ${PORT}'); 
+  console.log(`App is running on port ${PORT}`); 
 });
-
